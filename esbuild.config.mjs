86e29d489c2b8473372616fd20078bb7559ce511
takagino <178282378+highwoodfield@users.noreply.github.com>
2025-03-08 15:25:43 +0900

    Fix build

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -37,8 +37,7 @@ const context = await esbuild.context({
 	logLevel: "info",
 	sourcemap: prod ? false : "inline",
 	treeShaking: true,
-	outdir: ".",
-	// outfile: "main.js",
+	outfile: "main.js",
 	minify: prod,
 });
 
